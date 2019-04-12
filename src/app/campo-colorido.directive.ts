f4c985668f5ca63cbaee93f9efd2d9d0a0e2959e
Leandro Barros <leobarros.evangelista@gmail.com>
2019-04-12 17:26:32 -0300

    11.3. Vinculando propriedades do hospedeiro com @HostBinding.

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,23 +1,32 @@
-import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appCampoColorido]'
   // selector: '[input]' Caso queira colocar a diretiva personalizada em todos os inputs
 })
 export class CampoColoridoDirective {
+  
+  // O valor da váriavel corDeFundo vai ser atribuida a propriedade background de estilos, fazendo um binding, vinculando.
+  @HostBinding('style.backgroundColor') corDeFundo : string;
 
-  constructor(
-    private elementRef: ElementRef,
-    private renderer: Renderer2
-  ) { }
+  
+  // A parte comentada faz a mesma coisa;
+
+  // constructor(
+  //   private elementRef: ElementRef,
+  //   private renderer: Renderer2
+  // ) { }
+
 
   @HostListener('focus') aoGanharFoco() {
-    this.renderer.setStyle(this.elementRef.nativeElement,
-      'background-color', 'yellow');
+    this.corDeFundo = 'yellow';
+    // this.renderer.setStyle(this.elementRef.nativeElement,
+    //   'background-color', 'yellow');
   }
-
+  
   @HostListener('blur') aoPerdeFocus() {
-    this.renderer.setStyle(this.elementRef.nativeElement,
-      'background-color', 'transparent');
+    this.corDeFundo = 'transparent';
+    // this.renderer.setStyle(this.elementRef.nativeElement,
+    //   'background-color', 'transparent');
   }
 }
